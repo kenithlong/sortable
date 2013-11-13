cb0661ce120255e2e2cfc5419fb621fdc433d966
ken <kenithlong@gmail.com>
2013-11-12 23:25:18 -0500

    initial commit
    just a place to start

---
+++ sortable.py
@@ -0,0 +1,14 @@
+class Product(object):
+   def __init__(self, productName, manufacture, model, announceDate, family=None):
+      self.productName = productName
+      self.manufacture = manufacture
+      self.model = model
+
+
+def main():
+   #todo actually solve the problem eventually
+   pass
+
+if __name__ == "__main__":
+   main()
+
